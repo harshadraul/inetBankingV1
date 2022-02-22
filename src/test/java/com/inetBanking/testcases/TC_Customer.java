16661b40fed33eb920daa2dfb9cc40d39b2262b1
harshadraul <hbrpics@gmail.com>
2022-02-22 14:51:54 +0530

    added TC_search_customer and modified TC_Customer files

--- src/test/java/com/inetBanking/testcases/TC_Customer.java
+++ src/test/java/com/inetBanking/testcases/TC_Customer.java
@@ -1,1 +1,5 @@
 // this testcase is done by somebody else
+//this file is changed
+// some line are added 
+// file is modified
+
---
+++ src/test/java/com/inetBanking/testcases/TC_Search_Customer_005.java
@@ -0,0 +1,5 @@
+package com.inetBanking.testcases;
+
+public class TC_Search_Customer_005 {
+		// new file which is added 
+}
